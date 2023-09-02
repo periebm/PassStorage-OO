e6585b40f0a96f36773260f66b94eab345b2933e
Pericles <periclesebmendes@gmail.com>
2023-09-02 03:53:56 -0300

    change ignore

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000);
+  await app.listen(port, () => {
+    console.log('Server is up and running on port: ' + port);
+  });
 }
 bootstrap();
